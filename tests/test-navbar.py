4d171ad2c068bd968e0c5833b0fdce707ad3c571
Rahul Bajaj <rahulbajaj0242@gmail.com>
2023-04-27 15:31:58 -0400

    added test nav links while authenticated

--- tests/test-navbar.py
+++ tests/test-navbar.py
@@ -25,10 +25,10 @@ def logged_in_driver(driver):
 
 
 # Test cases for search home button
-def test_navbar_elements_unauthenticated(driver):
+def test_navbar_links_unauthenticated(driver):
   driver.get(website_url)
-  driver.find_element(By.XPATH, '//*[@id="basic-navbar-nav"]/div[2]/a[1]').click()
 
+  driver.find_element(By.XPATH, '//*[@id="basic-navbar-nav"]/div[2]/a[1]').click()
   WebDriverWait(driver, 10).until(EC.url_to_be(website_url + "register"))
   assert driver.current_url == (website_url + "register")
 
@@ -40,6 +40,30 @@ def test_navbar_elements_unauthenticated(driver):
   WebDriverWait(driver, 10).until(EC.url_to_be(website_url))
   assert driver.current_url == (website_url)
 
-  
+def test_navbar_links_authenticated(logged_in_driver):
+  logged_in_driver.get(website_url)
+
+  logged_in_driver.find_element(By.XPATH, '//*[@id="basic-navbar-nav"]/div[1]/a[2]').click()
+  WebDriverWait(logged_in_driver, 10).until(EC.url_to_be(website_url + "search"))
+  assert logged_in_driver.current_url == (website_url + "search")
+
+  logged_in_driver.find_element(By.XPATH, '//*[@id="basic-nav-dropdown"]').click()
+  logged_in_driver.implicitly_wait(5)
+
+  logged_in_driver.find_element(By.XPATH, '//*[@id="basic-navbar-nav"]/div[2]/div/div/a[1]').click()
+  WebDriverWait(logged_in_driver, 10).until(EC.url_to_be(website_url + "favourites"))
+  assert logged_in_driver.current_url == (website_url + "favourites")
+
+  logged_in_driver.find_element(By.XPATH, '//*[@id="basic-nav-dropdown"]').click()
+  logged_in_driver.implicitly_wait(5)
+  logged_in_driver.find_element(By.XPATH, '//*[@id="basic-navbar-nav"]/div[2]/div/div/a[2]').click()
+  WebDriverWait(logged_in_driver, 10).until(EC.url_to_be(website_url + "history"))
+  assert logged_in_driver.current_url == (website_url + "history")
+
+  logged_in_driver.find_element(By.XPATH, '//*[@id="basic-nav-dropdown"]').click()
+  logged_in_driver.implicitly_wait(5)
+  logged_in_driver.find_element(By.XPATH, '//*[@id="basic-navbar-nav"]/div[2]/div/div/a[3]').click()
+  WebDriverWait(logged_in_driver, 10).until(EC.url_to_be(website_url + "login"))
+  assert logged_in_driver.current_url == (website_url + "login")
 
 
