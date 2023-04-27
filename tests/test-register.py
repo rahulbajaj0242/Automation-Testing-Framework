996c3f1f54eb279d8c6677835c07144bf1020c38
Rahul Bajaj <rahulbajaj0242@gmail.com>
2023-04-27 13:08:10 -0400

    all tests added and working

--- tests/test-register.py
+++ tests/test-register.py
@@ -20,5 +20,31 @@ def unique_username():
 
 
 # Test cases for register form
-def test_successful_register(driver):
-  pass
+def test_successful_register(driver, unique_username):
+  driver.get(webiste_url + "register")
+  driver.find_element(By.XPATH, '//*[@id="userName"]').send_keys(unique_username)
+  driver.find_element(By.XPATH, '//*[@id="password"]').send_keys("test")
+  driver.find_element(By.XPATH, '//*[@id="confirmPassword"]').send_keys("test")
+  driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/form/button').click()
+  time.sleep(3)
+  assert driver.current_url == (webiste_url + "login")
+
+def test_unsuccessful_register_username_exist(driver):
+  driver.get(webiste_url + "register")
+  driver.find_element(By.XPATH, '//*[@id="userName"]').send_keys('test-user')
+  driver.find_element(By.XPATH, '//*[@id="password"]').send_keys("test")
+  driver.find_element(By.XPATH, '//*[@id="confirmPassword"]').send_keys("test")
+  driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/form/button').click()
+  time.sleep(1)
+  alert_text = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/form/div[4]/div[2]').text
+  assert alert_text == "User Name already taken"
+
+def test_unsuccessful_register_password_not_match(driver, unique_username):
+  driver.get(webiste_url + "register")
+  driver.find_element(By.XPATH, '//*[@id="userName"]').send_keys(unique_username)
+  driver.find_element(By.XPATH, '//*[@id="password"]').send_keys("test")
+  driver.find_element(By.XPATH, '//*[@id="confirmPassword"]').send_keys("tes")
+  driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/form/button').click()
+  time.sleep(1)
+  alert_text = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/form/div[4]/div[2]').text
+  assert alert_text == "Passwords do not match"
