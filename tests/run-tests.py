a998a76c2db41aaf71efcd1019d4ca05abd20c3e
Rahul Bajaj <rahulbajaj0242@gmail.com>
2023-04-28 13:45:22 -0400

    added test-history in run_tests

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -1,7 +1,7 @@
 import pytest
 
 def run_tests():
-    pytest.main(['test-home-page.py', 'test-login.py', 'test-navbar.py', 'test-register.py'])
+    pytest.main(['test-home-page.py', 'test-login.py', 'test-navbar.py', 'test-register.py', 'test-history'])
 
 
 if __name__ == '__main__':
